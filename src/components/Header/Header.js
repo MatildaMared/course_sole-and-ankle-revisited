ff929bf3d06c99a6679d1d7dd0774e666f7041cd
Matilda Mared <matildamared@live.se>
2022-05-08 15:34:58 +0200

    complete exercise 2 - making the header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,73 +1,120 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import { COLORS, WEIGHTS, BREAKPOINTS } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
+import MobileMenu from "../MobileMenu";
+import Icon from "../Icon";
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+	const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
+	// For our mobile hamburger menu, we'll want to use a button
+	// with an onClick handler, something like this:
+	//
+	// <button onClick={() => setShowMobileMenu(true)}>
 
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Side>
-          <Logo />
-        </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
-      </MainHeader>
+	return (
+		<header>
+			<SuperHeader />
+			<MainHeader>
+				<Side>
+					<Logo />
+				</Side>
+				<Nav>
+					<NavLink href="/sale">Sale</NavLink>
+					<NavLink href="/new">New&nbsp;Releases</NavLink>
+					<NavLink href="/men">Men</NavLink>
+					<NavLink href="/women">Women</NavLink>
+					<NavLink href="/kids">Kids</NavLink>
+					<NavLink href="/collections">Collections</NavLink>
+				</Nav>
+				<MobileNav>
+					<Button>
+						<Icon id="shopping-bag" />
+					</Button>
+					<Button>
+						<Icon id="search" />
+					</Button>
+					<Button>
+						<Icon
+							id={showMobileMenu ? "close" : "menu"}
+							onClick={() => setShowMobileMenu(!showMobileMenu)}
+						/>
+					</Button>
+				</MobileNav>
+				<Side />
+			</MainHeader>
 
-      <MobileMenu
-        isOpen={showMobileMenu}
-        onDismiss={() => setShowMobileMenu(false)}
-      />
-    </header>
-  );
+			<MobileMenu
+				isOpen={showMobileMenu}
+				onDismiss={() => setShowMobileMenu(false)}
+			/>
+		</header>
+	);
 };
 
 const MainHeader = styled.div`
-  display: flex;
-  align-items: baseline;
-  padding: 18px 32px;
-  height: 72px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+	display: flex;
+	align-items: baseline;
+	padding: 18px 32px;
+	height: 72px;
+	border-bottom: 1px solid ${COLORS.gray[300]};
+
+	@media (max-width: ${BREAKPOINTS.tablet}) {
+		border-top: 4px solid ${COLORS.gray[900]};
+	}
 `;
 
 const Nav = styled.nav`
-  display: flex;
-  gap: 48px;
-  margin: 0px 48px;
+	display: flex;
+	gap: 48px;
+	margin: 0px 48px;
+
+	@media (max-width: ${BREAKPOINTS.tablet}) {
+		display: none;
+	}
+`;
+
+const Button = styled.button`
+	border: none;
+	background-color: transparent;
+	cursor: pointer;
+	padding: 0;
+	color: black;
+`;
+
+const MobileNav = styled.nav`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	gap: 16px;
+
+	@media (min-width: ${BREAKPOINTS.mobile}) {
+		gap: 24px;
+	}
+
+	@media (min-width: ${BREAKPOINTS.tablet}) {
+		display: none;
+	}
 `;
 
 const Side = styled.div`
-  flex: 1;
+	flex: 1;
 `;
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+	font-size: 1.125rem;
+	text-transform: uppercase;
+	text-decoration: none;
+	color: ${COLORS.gray[900]};
+	font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
+	&:first-of-type {
+		color: ${COLORS.secondary};
+	}
 `;
 
 export default Header;
